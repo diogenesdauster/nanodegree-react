a8600c38b775253c9572921689736b57436da5b1
disias <diogenes.dauster@gmail.com>
2018-04-29 14:36:49 -0300

    aplicando o conceito de desestruturação de objetos do ES6 para extrair o conteudo do props e do state.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -20,13 +20,16 @@ class ListContacts extends Component{
     }
 
     render(){
+        const {contacts, onDeleteContact} =  this.props
+        const {query} = this.state
+
         let showingContacts
   
         if(this.state.query){
             const match = new RegExp(escapeRegExp(this.state.query),'i')
-            showingContacts = this.props.contacts.filter( (contact) => match.test(contact.name) )
+            showingContacts = contacts.filter( (contact) => match.test(contact.name) )
         }else{
-            showingContacts = this.props.contacts
+            showingContacts = contacts
         }
 
         showingContacts.sort(sortBy('name'))
@@ -38,7 +41,7 @@ class ListContacts extends Component{
                         className='search-contacts'
                         type='text'
                         placeholder='Search contacts'
-                        value={this.state.query}
+                        value={query}
                         onChange={(event) => this.updateQuery(event.target.value)} />
                 </div>
                 <ol className='contact-list'>
@@ -51,7 +54,7 @@ class ListContacts extends Component{
                                 <p>{contact.name}</p>
                                 <p>{contact.email}</p>
                             </div>
-                            <button onClick={() => this.props.onDeleteContact(contact) } className='contact-remove'>
+                            <button onClick={() => onDeleteContact(contact) } className='contact-remove'>
                                 Remove
                             </button>
                         </li>

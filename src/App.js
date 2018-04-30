0a7d14463292e0807cab12132044f033da843838
disias <diogenes.dauster@gmail.com>
2018-04-29 21:11:41 -0300

    criando um novo componente chamado CreateContact e adicionado um novo atributo ao estado do app componente para fazer a transição de tela entre ListContacts e o CreateContact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
-
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
+    screen:'create',
     contacts: []
   }
 
@@ -25,7 +26,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+      {this.state.screen == 'list' &&(
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+      )}
+
+      {this.state.screen == 'create' &&(
+        <CreateContact/>
+      )}
+       
       </div>
     );
   }
---
+++ src/CreateContact.js
@@ -0,0 +1,13 @@
+import React ,{Component} from 'react'
+
+
+class CreateContact extends Component {
+    render(){
+        return(
+            <div>Hello CreateContact</div>
+        )
+    }
+}
+
+
+export default CreateContact;

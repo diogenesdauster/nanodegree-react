6eff66312d11bf9877df2421d7815f24abe6826c
disias <diogenes.dauster@gmail.com>
2018-06-10 15:21:04 -0300

    utilizando a função connect do react-redux para mapear o estado para a props do componente usando currying

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
-import { addRecipe } from "../actions";
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
+    console.log("Props", this.props);
     return <div>Hello World !!</div>;
   }
 }
-export default App;
+
+function mapStateToProps(calender) {
+  const dayOrder = [
+    "sunday",
+    "monday",
+    "tueday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+  ];
+
+  return {
+    calender: dayOrder.map(day => ({
+      day,
+      meals: Object.keys(calender[day]).reduce((meals, meal) => {
+        meals[day] = calender[day][meal] ? calender[day][meal] : null;
+        return meals;
+      }, {})
+    }))
+  };
+}
+
+export default connect(mapStateToProps)(App);

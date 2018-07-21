23deb6b4a87df8d122f10ba6e10023f6efe76257
disias <diogenes.dauster@gmail.com>
2018-07-21 18:56:04 -0300

    refact: add component AddEntry to Redux using connect and mapStateToProps functions for calling dispatch function pass actions

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
-import { getMetricMetaInfo, timeToString } from "../utils/helpers";
+import {
+  getMetricMetaInfo,
+  timeToString,
+  getDailyReminderValue
+} from "../utils/helpers";
 import UdacitySlider from "./UdacitySlider";
 import UdacitySteppers from "./UdacitySteppers";
 import DateHeader from "./DateHeader";
 import Ionicons from "../node_modules/@expo/vector-icons/Ionicons";
 import TextButton from "./TextButton";
 import { submit, reset } from "../utils/api";
+import { connect } from "react-redux";
+import { addEntry } from "../actions";
 
 function SubmitBtn({ onPress }) {
   return (
@@ -16,7 +22,7 @@ function SubmitBtn({ onPress }) {
   );
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -60,6 +66,12 @@ export default class AddEntry extends Component {
     const key = timeToString();
     const entry = this.state;
 
+    this.props.dispatch(
+      addEntry({
+        [key]: entry
+      })
+    );
+
     this.setState({
       run: 0,
       bike: 0,
@@ -73,6 +85,13 @@ export default class AddEntry extends Component {
 
   reset = () => {
     const key = timeToString();
+
+    this.props.dispatch(
+      addEntry({
+        [key]: getDailyReminderValue()
+      })
+    );
+
     reset(key);
   };
 
@@ -121,3 +140,12 @@ export default class AddEntry extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  const key = timeToString();
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === "undefined"
+  };
+}
+
+export default connect(mapStateToProps)(AddEntry);

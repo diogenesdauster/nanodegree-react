0b7796b0e86421757001167de528a634c4162a7d
disias <diogenes.dauster@gmail.com>
2018-07-21 18:31:54 -0300

    feat: add redux and react-redux for create store and provider that store for all app

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet, Slider } from "react-native";
 import AddEntry from "./components/AddEntry";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import recuder from "./recuders";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry />
-      </View>
+      <Provider store={createStore(recuder)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
     );
   }
 }

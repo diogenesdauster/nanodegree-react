74446de8ab2d44fb61fa4f4fc1ac958ebdc22a47
disias <diogenes.dauster@gmail.com>
2018-07-29 22:00:42 -0300

    feat: add permission location for Live component and handle the current location

--- components/Live.js
+++ components/Live.js
@@ -32,7 +32,19 @@ export default class Live extends Component {
       });
   }
 
-  askPermisson = () => {};
+  askPermisson = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === "granted") {
+          return this.setLocation();
+        }
+        this.setState(() => ({ status }));
+      })
+      .catch(error => {
+        console.warn("error asking Location permissions : ", error);
+        this.setState({ status: "undetermined" });
+      });
+  };
   setLocation = () => {
     Location.watchPositionAsync(
       {
@@ -88,16 +100,20 @@ export default class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>North</Text>
+          <Text style={styles.direction}>{direction}</Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Altitude</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{200} Feet</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {Math.round(coords.altitude * 3.2808)} Feet
+            </Text>
           </View>
           <View style={styles.metric}>
             <Text style={[styles.header, { color: white }]}>Speed</Text>
-            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+            <Text style={[styles.subHeader, { color: white }]}>
+              {(coords.speed * 2.2369).toFixed(1)} MPH
+            </Text>
           </View>
         </View>
       </View>

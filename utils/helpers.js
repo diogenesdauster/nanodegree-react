ffe5eb8e932d09b469244c5944c446eb6e4397ae
disias <diogenes.dauster@gmail.com>
2018-07-30 17:20:53 -0300

    feat: create Local Notification for remebering the user make your log data

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, AsyncStorage } from "react-native";
 import {
   FontAwesome,
   MaterialIcons,
   MaterialCommunityIcons
 } from "@expo/vector-icons";
 import { white, red, orange, blue, lightPurp, pink } from "./colors";
+import { Notifications, Permissions } from "expo";
+
+const NOTIFICATION_KEY = "UdacityFitness:notifications";
 
 export function getMetricMetaInfo(metric) {
   const info = {
@@ -144,4 +147,50 @@ export function getDailyReminderValue() {
   return {
     today: "👋🏻 Don't forget to log your data today"
   };
+}
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+    Notifications.cancelAllScheduledNotificationsAsync()
+  );
+}
+
+function createNotification() {
+  return {
+    title: "Log your status",
+    body: "👋🏻 Don't forget to log your data today",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: "high",
+      stick: false,
+      vibrate: true
+    }
+  };
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
+          if (status === "granted") {
+            Notifications.cancelAllScheduledNotificationsAsync();
+            let tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            tomorrow.setHours(20);
+            tomorrow.setMinutes(0);
+
+            Notifications.scheduleLocalNotificationAsync(createNotification(), {
+              time: tomorrow,
+              repeat: "day"
+            });
+
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+          }
+        });
+      }
+    });
 }
